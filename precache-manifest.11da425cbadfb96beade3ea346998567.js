f7195382c536e421a5957a6b967d7d6d36126021
Artem <pankoartem99@gmail.com>
2020-10-02 14:08:03 +0300

    Updates

---
+++ precache-manifest.11da425cbadfb96beade3ea346998567.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33018a84e07c6e223ccaa4bf48e6ca68",
+    "url": "/with-yelp/index.html"
+  },
+  {
+    "revision": "c0c49ca00d3cac974f52",
+    "url": "/with-yelp/static/css/main.d9cdcca2.chunk.css"
+  },
+  {
+    "revision": "42d093efd3f58a10f346",
+    "url": "/with-yelp/static/js/2.7e171355.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/with-yelp/static/js/2.7e171355.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0c49ca00d3cac974f52",
+    "url": "/with-yelp/static/js/main.e2e0b41e.chunk.js"
+  },
+  {
+    "revision": "8e56a052c4fa3e6230d8",
+    "url": "/with-yelp/static/js/runtime-main.8ae602d5.js"
+  }
+]);
